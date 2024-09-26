5a300460740aa0f06a2fff3acddc7999493d79b7
Harichandra-Prasath <1hcp302004@gmail.com>
2024-09-26 15:31:40 +0530

    Ping google

--- Watcher.go
+++ Watcher.go
@@ -7,7 +7,7 @@ import (
 	"time"
 )
 
-// can be change for user preference
+// Can be changed for user preference
 var Duration time.Duration = 10 * time.Minute
 
 func Monitor(CONN_STATUS *bool) {
@@ -39,12 +39,12 @@ func checkInteface(PREV_CONN_STATUS *bool) {
 
 	res := string(stdout)
 
-	if strings.Contains(res, "unavailable") {
+	if strings.Contains(res, "unavailable") { // Plugged out
 		slog.Info("Ethernet Interface is  Unavailable")
 		if *(PREV_CONN_STATUS) {
 			*(PREV_CONN_STATUS) = false
 		}
-	} else if strings.Contains(res, "disconnected") {
+	} else if strings.Contains(res, "disconnected") { // similar to ethernet connection is set to off
 		slog.Info("Ethernet Interface is Available but Disconnected")
 		if *(PREV_CONN_STATUS) {
 			*(PREV_CONN_STATUS) = false
@@ -52,11 +52,30 @@ func checkInteface(PREV_CONN_STATUS *bool) {
 	} else {
 		slog.Info("Ethernet Inteface is Available and Connected")
 		if !*(PREV_CONN_STATUS) {
-			*(PREV_CONN_STATUS) = true
-			slog.Debug("Attempting to send Mail to notify")
-			go SendMail()
+			if checkPing() {
+				*(PREV_CONN_STATUS) = true
+				slog.Debug("Attempting to send Mail to notify")
+				go SendMail()
+			}
+
 		}
 
 	}
 
+}
+
+// This function try to ping google.com
+// some times there may be connection avaialble but no internet
+func checkPing() bool {
+	cmd := exec.Command("ping", "google.com", "-c", "2", "-w", "2")
+	res, err := cmd.Output()
+	if err != nil {
+		slog.Error("Error Occured in Ping", "Error", err)
+		return false
+	}
+	if strings.Contains(string(res), "service not known") {
+		slog.Info("Internet Not Available")
+		return false
+	}
+	return true
 }
